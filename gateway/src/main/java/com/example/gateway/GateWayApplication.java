fdaaf1826f35eee3ca2fc4c7491e597ee9348727
‘’dhia’’ <meddhiadin.boudali@esprit.tngit config --global user.email meddhiadin.boudali@esprit.tn>
2023-12-13 23:04:38 +0100

    dhia

--- gateway/src/main/java/com/example/gateway/GateWayApplication.java
+++ gateway/src/main/java/com/example/gateway/GateWayApplication.java
@@ -2,16 +2,14 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
+public class GateWayApplication {
 
-
-public class EnigmaGatewayApp {
-    public static void main(String[] args) { SpringApplication.run(EnigmaGatewayApp.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
     }
+
 }
